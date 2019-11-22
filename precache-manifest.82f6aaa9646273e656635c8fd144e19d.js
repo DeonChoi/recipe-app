bfa31ce1c0d504151555d68051b34ee45a795121
Deon Choi <deon.choi@gmail.com>
2019-11-21 23:32:42 -0800

    Updates

---
+++ precache-manifest.82f6aaa9646273e656635c8fd144e19d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b96611d7aab1a1c5d57a1b346ce5836",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fe17f5829d201b3bca6b",
+    "url": "/recipe-app/static/css/main.b36eb670.chunk.css"
+  },
+  {
+    "revision": "72940ad35602f1810dc6",
+    "url": "/recipe-app/static/js/2.dba5fc78.chunk.js"
+  },
+  {
+    "revision": "fe17f5829d201b3bca6b",
+    "url": "/recipe-app/static/js/main.b713619a.chunk.js"
+  },
+  {
+    "revision": "f45dcbc11275cd88eb53",
+    "url": "/recipe-app/static/js/runtime-main.3a5d6ccd.js"
+  },
+  {
+    "revision": "4e85163a49983fec0996fa58935c5f1e",
+    "url": "/recipe-app/static/media/background.4e85163a.png"
+  },
+  {
+    "revision": "7de2ed3a9a9ee918be6856644eda3259",
+    "url": "/recipe-app/static/media/saveMoney.7de2ed3a.jpg"
+  },
+  {
+    "revision": "9c2bd879c61f306373f4f111a97c66e9",
+    "url": "/recipe-app/static/media/searchBackground.9c2bd879.jpg"
+  },
+  {
+    "revision": "1da1b2662c7e8575640e838fe297507e",
+    "url": "/recipe-app/static/media/stayConnected.1da1b266.jpg"
+  },
+  {
+    "revision": "49dbd741d4dc35ba1fc2db6e7df60f56",
+    "url": "/recipe-app/static/media/stayHealthy.49dbd741.jpg"
+  }
+]);
